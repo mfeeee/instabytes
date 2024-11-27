bfa6a750d907a7628c0642de8b3d481cb978613d
mfeeee <mfernandarocosta@gmail.com>
2024-11-26 21:58:31 -0300

    Estruturando e modularizando a API

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
+import routes from "./src/routes/postsRoutes.js";
 
-const posts = [
+/*const posts = [
   {
     id: 1,
     descricao: "Uma foto teste",
@@ -31,20 +32,18 @@ const posts = [
     descricao: "Gatinho ronronando no colo",
     imagem: "https://placekitten.com/200/200",
   },
-];
+];*/
 
 const app = express();
-app.use(express.json());
+routes(app);
 
 app.listen(3000, () => {
     console.log("Servidor escutando...");
 });
 
-app.get("/posts", (req, res) => {
-    res.status(200).json(posts);
-});
 
-function buscarPostPorID(id) {
+
+/*function buscarPostPorID(id) {
     return posts.findIndex((post) =>{
         return post.id === Number(id);
     });
@@ -53,4 +52,4 @@ function buscarPostPorID(id) {
 app.get("/posts/:id", (req, res) => {
     const index = buscarPostPorID(req.params.id)
     res.status(200).json(posts[index]);
-});
+});*/
